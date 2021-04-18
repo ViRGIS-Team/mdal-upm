a26170da41c41c70c362f741164f4e88b515ff97
Paul Harwood <runette"gmail.com>
2021-04-18 23:03:52 +0100

    fixed for linux

--- Assets/Scripts/install_scripts.cs
+++ Assets/Scripts/install_scripts.cs
@@ -102,7 +102,7 @@ namespace Mdal {
                                                     $"-test {test}";
 #else
                 compiler.StartInfo.FileName = "/bin/bash";
-                compiler.StartInfo.Arguments = $" -l  \"{Path.Combine(path, "install_script.sh")}\" " +
+                compiler.StartInfo.Arguments = $" -i \"{Path.Combine(path, "install_script.sh")}\" " +
                                                 "-p mdal " +
                                                 $"-i {install} " +
                                                 $"-d '{pluginPath}' " +
