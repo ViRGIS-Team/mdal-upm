481bd1fcf5ef179dcf7b5c1625a3a1f45f9165a7
Paul <runette@gmail.com>
2021-07-22 19:27:34 +0100

    windows updates

--- Editor/Plugins/install_script.ps1
+++ Editor/Plugins/install_script.ps1
@@ -5,18 +5,31 @@ Param(
     [string]$shared_assets
     )
 
-conda install -c conda-forge --prefix $destination --copy  --mkdir $install -y
+$logfile = -join($destination, "\mdal_log.txt")
+
+$outp = conda install -c conda-forge --prefix "$destination" --copy  --mkdir $install -y -vv *>&1 
+
+Set-Location $destination
+$temp = [System.IO.Path]::GetFileName((Get-Location).toString()) 
+
+if ( $temp -ne "Conda")
+{
+    Write-Output "Working Directory Invalid" (Get-Location).tostring() >> "$logfile"
+    Exit
+}
+
+Write-Output $outp >> "$logfile"
 
 # Move the shared data to the shared assets folder to esnure that it gets built into the client
 
 Write-Output "Processing gdal data"
 $file = -join($destination, "\Library\share\gdal")
-Write-Output "Copy $file to $shared_assets"
+Write-Output "Copy $file to $shared_assets" >> "$logfile"
 Move-Item -Path $file -Destination $shared_assets  -Force
 
 Write-Output "Processing proj data"
 $file = -join($destination, "\Library\share\proj")
-Write-Output "Copy $file to $shared_assets"
+Write-Output "Copy $file to $shared_assets"  >> "$logfile"
 Move-Item -Path $file -Destination $shared_assets  -Force
 
 # Tree shakering
@@ -25,7 +38,7 @@ Set-Location $destination
 
 Remove-Item *.dll
 
-Get-ChildItem -exclude .*, conda-meta, *.meta, Library | Remove-Item -Recurse
+Get-ChildItem -exclude .*, conda-meta, *.meta, Library, *.txt | Remove-Item -Recurse
 
 Set-Location Library
 
