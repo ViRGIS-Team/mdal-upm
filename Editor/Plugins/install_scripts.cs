a3399e0fbec7bf566f9084dc7865fc51217692df
Paul <runette@gmail.com>
2022-02-01 12:30:44 +0000

    Update install_scripts.cs

--- Editor/Plugins/install_scripts.cs
+++ Editor/Plugins/install_scripts.cs
@@ -9,7 +9,7 @@ namespace Mdal {
 
     public class Install{
 
-        const string packageVersion = "0.9.1";
+        const string packageVersion = "0.9.3";
 
         [InitializeOnLoadMethod]
         static void OnProjectLoadedinEditor()
